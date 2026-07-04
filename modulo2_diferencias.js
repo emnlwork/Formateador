10f80500d26e54b293d54e3eb9ec2b2528ec1614
emnlwork <emanuelcontructorag6@gmail.com>
2026-07-03 22:35:13 -0700

    vrsn

--- modulo2_diferencias.js
+++ modulo2_diferencias.js
@@ -27,7 +27,7 @@
             <div class="row" style="justify-content:space-between;">
                 <h3><i class="fas fa-balance-scale"></i> Comparar folios múltiples</h3>
                 <div style="display:flex; align-items:center; gap:0.8rem;">
-                    <span style="font-size:0.7rem; color:var(--grayl); background:rgba(0,0,0,0.3); padding:0.15rem 0.5rem; border-radius:3px; border:1px solid var(--blu);">v3.0</span>
+                    <span style="font-size:0.7rem; color:var(--grayl); background:rgba(0,0,0,0.3); padding:0.15rem 0.5rem; border-radius:3px; border:1px solid var(--blu);">v3.0b</span>
                     <button class="clear-module-btn"><i class="fas fa-eraser"></i> Limpiar</button>
                 </div>
             </div>
